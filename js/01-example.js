c2e72086dce641399e1756a8947fc42900a9b7bd
licaret <licaretraul@gmail.com>
2023-11-13 22:04:24 +0200

    Add exercices

---
+++ js/01-example.js
@@ -0,0 +1,8 @@
+// Display the total number of apples and grapes on the screen. The difference between apples and grapes.
+
+const apples = Number.parseInt(prompt("Please insert the number of apples"));
+const grapes = Number.parseInt(prompt("Please insert the number of grapes"));
+const total = apples + grapes;
+console.log("total:", total);
+const diff = apples - grapes;
+console.log("diff:", diff);
